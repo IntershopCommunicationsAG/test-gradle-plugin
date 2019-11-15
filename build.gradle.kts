3ce8e96e6e9de96c5ccf43450c29eabf43889bb2
Matthias Raab <m.raab@intershop.de>
2019-11-15 13:30:35 +0100

    extend test for kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 import com.jfrog.bintray.gradle.BintrayExtension
-import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
-import org.asciidoctor.gradle.AsciidoctorTask
-import org.asciidoctor.gradle.AsciidoctorExtension
+import org.asciidoctor.gradle.jvm.AsciidoctorTask
 import java.util.Date
 
 plugins {
@@ -14,7 +12,7 @@ plugins {
     idea
 
     // plugin for documentation
-    id("org.asciidoctor.convert") version "1.5.9.2"
+    id("org.asciidoctor.jvm.convert") version "2.3.0"
 
     // publish plugin
     `maven-publish`
@@ -26,7 +24,12 @@ plugins {
 group = "com.intershop.gradle.test"
 description = "Gradle test library - test extension for Gradle plugin builds"
 
-version = "3.1.0-dev.2"
+version = "3.4.0"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -38,65 +41,58 @@ if (project.version.toString().endsWith("-SNAPSHOT")) {
     status = "snapshot'"
 }
 
-
-
-tasks.withType<Test>().configureEach {
-    testLogging.showStandardStreams = true
-
-    // Gradle versions for test
-    if (! System.getProperty("GRADLETEST_VERSION").isNullOrEmpty()) {
-        systemProperty("intershop.gradle.versions", System.getProperty("GRADLETEST_VERSION"))
-    } else {
-        systemProperty("intershop.gradle.versions", "4.9,5.0-rc-3")
-    }
-
-    systemProperty("intershop.test.base.dir", (File(project.buildDir, "test-working")).absolutePath)
-}
-
-task("copyAsciiDoc") {
-
-    val outputDir = file("${buildDir}/tmp/asciidoctorSrc")
-    val inputFiles = fileTree(mapOf("dir" to rootDir, "include" to listOf("**/*.asciidoc")))
-
-    inputs.files.plus( inputFiles )
-    outputs.dir( outputDir )
-
-    doLast {
-        outputDir.mkdir()
-
-        project.copy {
-            from(inputFiles)
-            into(outputDir)
-        }
-    }
-}
-
-configure<AsciidoctorExtension> {
-    noDefaultRepositories = true
-}
-
 tasks {
+    withType<Test>().configureEach {
+        testLogging.showStandardStreams = true
+
+        // Gradle versions for test
+        systemProperty("intershop.gradle.versions", "5.6.4,6.0")
+        systemProperty("intershop.test.base.dir", (File(project.buildDir, "test-working")).absolutePath)
+    }
+
+    val copyAsciiDoc = register<Copy>("copyAsciiDoc") {
+        includeEmptyDirs = false
+
+        val outputDir = file("$buildDir/tmp/asciidoctorSrc")
+        val inputFiles = fileTree(mapOf("dir" to rootDir,
+            "include" to listOf("**/*.asciidoc"),
+            "exclude" to listOf("build/**")))
+
+        inputs.files.plus( inputFiles )
+        outputs.dir( outputDir )
+
+        doFirst {
+            outputDir.mkdir()
+        }
+
+        from(inputFiles)
+        into(outputDir)
+    }
+
     withType<AsciidoctorTask> {
         dependsOn("copyAsciiDoc")
 
-        sourceDir = file("${buildDir}/tmp/asciidoctorSrc")
+        setSourceDir(file("$buildDir/tmp/asciidoctorSrc"))
         sources(delegateClosureOf<PatternSet> {
             include("README.asciidoc")
         })
 
-        backends("html5", "docbook")
+        outputOptions {
+            setBackends(listOf("html5", "docbook"))
+        }
+
         options = mapOf( "doctype" to "article",
-                         "ruby"    to "erubis")
+            "ruby"    to "erubis")
         attributes = mapOf(
-                "latestRevision"        to  project.version,
-                "toc"                   to "left",
-                "toclevels"             to "2",
-                "source-highlighter"    to "coderay",
-                "icons"                 to "font",
-                "setanchors"            to "true",
-                "idprefix"              to "asciidoc",
-                "idseparator"           to "-",
-                "docinfo1"              to "true")
+            "latestRevision"        to  project.version,
+            "toc"                   to "left",
+            "toclevels"             to "2",
+            "source-highlighter"    to "coderay",
+            "icons"                 to "font",
+            "setanchors"            to "true",
+            "idprefix"              to "asciidoc",
+            "idseparator"           to "-",
+            "docinfo1"              to "true")
     }
 
     withType<JacocoReport> {
@@ -119,28 +115,11 @@ tasks {
     getByName("publishToMavenLocal")?.dependsOn("asciidoctor")
 }
 
-val sourcesJar = task<Jar>("sourceJar") {
-    description = "Creates a JAR that contains the source code."
-
-    from(sourceSets.getByName("main").allSource)
-    classifier = "sources"
-}
-
-val groovydocJar = task<Jar>("javadocJar") {
-    dependsOn("groovydoc")
-    description = "Creates a JAR that contains the javadocs."
-
-    from(tasks.getByName("groovydoc"))
-    classifier = "javadoc"
-}
-
 publishing {
     publications {
         create("intershopMvn", MavenPublication::class.java) {
 
             from(components["java"])
-            artifact(sourcesJar)
-            artifact(groovydocJar)
 
             artifact(File(buildDir, "asciidoc/html5/README.html")) {
                 classifier = "reference"
@@ -150,24 +129,29 @@ publishing {
                 classifier = "docbook"
             }
 
-            pom.withXml {
-                val root = asNode()
-                root.appendNode("name", project.name)
-                root.appendNode("description", project.description)
-                root.appendNode("url", "https://github.com/IntershopCommunicationsAG/${project.name}")
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set("https://github.com/IntershopCommunicationsAG/${project.name}")
 
-                val scm = root.appendNode( "scm" )
-                scm.appendNode( "url", "https://github.com/IntershopCommunicationsAG/${project.name}")
-                scm.appendNode( "connection", "git@github.com:IntershopCommunicationsAG/${project.name}.git")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
 
-                val org = root.appendNode( "organization" )
-                org.appendNode( "name", "Intershop Communications" )
-                org.appendNode( "url", "http://intershop.com" )
+                scm {
+                    connection.set("https://github.com/IntershopCommunicationsAG/${project.name}.git")
+                    developerConnection.set("git@github.com:IntershopCommunicationsAG/${project.name}.git")
+                    url.set("https://github.com/IntershopCommunicationsAG/${project.name}")
+                }
 
-                val license = root.appendNode( "licenses" ).appendNode( "license" );
-                license.appendNode( "name", "Apache License, Version 2.0" )
-                license.appendNode( "url", "http://www.apache.org/licenses/LICENSE-2.0" )
-                license.appendNode( "distribution", "repo" )
+                organization {
+                    name.set("Intershop Communications AG")
+                    url.set("http://intershop.com")
+                }
             }
         }
     }
@@ -208,13 +192,13 @@ repositories {
 }
 
 dependencies {
-    compile("org.spockframework:spock-core:1.2-groovy-2.5") {
+    implementation("org.spockframework:spock-core:1.2-groovy-2.5") {
         exclude(group = "org.codehaus.groovy")
     }
 
-    compile("commons-io:commons-io:2.2")
-    compile("com.sun.xml.bind:jaxb-impl:2.2.3")
+    implementation("commons-io:commons-io:2.2")
+    implementation("com.sun.xml.bind:jaxb-impl:2.2.3")
 
-    compile(gradleTestKit())
+    implementation(gradleTestKit())
 }
 
