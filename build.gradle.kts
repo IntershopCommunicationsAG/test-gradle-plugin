ea2497f34314d1c04829989e17c98ab4e5c46203
skiesewetter-intershop <82586807+skiesewetter-intershop@users.noreply.github.com>
2025-08-15 11:38:14 +0200

    ci: migrate publishing to maven central (#106686)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.asciidoctor.gradle.jvm.AsciidoctorTask
+import io.gitee.pkmer.enums.PublishingType
 
 /*
  * Copyright 2022 Intershop Communications AG.
@@ -36,6 +37,8 @@ plugins {
 
     // artifact signing - necessary on Maven Central
     signing
+
+    id("io.gitee.pkmer.pkmerboot-central-publisher") version "1.1.1"
 }
 
 group = "com.intershop.gradle.test"
@@ -46,6 +49,11 @@ version = projectVersion ?: "LOCAL"
 
 val sonatypeUsername: String? by project
 val sonatypePassword: String? by project
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
 
 java {
     withJavadocJar()
@@ -150,6 +158,8 @@ tasks {
     getByName("jar").dependsOn("asciidoctor")
 }
 
+val stagingRepoDir = project.layout.buildDirectory.dir("stagingRepo")
+
 publishing {
     publications {
         create("intershopMvn", MavenPublication::class.java) {
@@ -163,7 +173,8 @@ publishing {
             artifact(project.layout.buildDirectory.file("docs/asciidoc/docbook/README.xml")) {
                 classifier = "docbook"
             }
-
+        }
+        withType<MavenPublication>().configureEach {
             pom {
                 name.set(project.name)
                 description.set(project.description)
@@ -195,24 +206,34 @@ publishing {
         }
     }
     repositories {
-        maven {
-            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
-            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-            credentials {
-                username = sonatypeUsername
-                password = sonatypePassword
+        repositories {
+            maven {
+                name = "LOCAL"
+                url = stagingRepoDir.get().asFile.toURI()
             }
         }
     }
 }
 
-signing {
-    sign(publishing.publications["intershopMvn"])
+pkmerBoot {
+    sonatypeMavenCentral{
+        // the same with publishing.repositories.maven.url in the configuration.
+        stagingRepository = stagingRepoDir
+
+        /**
+         * get username and password from
+         * <a href="https://central.sonatype.com/account"> central sonatype account</a>
+         */
+        username = sonatypeUsername
+        password = sonatypePassword
+
+        // Optional the publishingType default value is PublishingType.AUTOMATIC
+        publishingType = PublishingType.USER_MANAGED
+    }
 }
 
-repositories {
-    mavenCentral()
+signing {
+    sign(publishing.publications["intershopMvn"])
 }
 
 dependencies {
